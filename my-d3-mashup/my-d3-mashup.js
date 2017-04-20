b9cec24a79cd44533833060fdcf886efded5a4f2
Kevin McGovern <kmcg82004@gmail.com>
2017-04-20 16:30:27 -0400

    d3 chart with working hypercube

---
+++ my-d3-mashup/my-d3-mashup.js
@@ -0,0 +1,217 @@
+/*
+ * Bootstrap-based responsive mashup
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+//to avoid errors in workbench: you can remove this when you have added an app
+var app;
+require.config( {
+	baseUrl: (config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "" ) + config.prefix + "resources"
+} );
+
+require( ["js/qlik", "//d3js.org/d3.v4.min.js", "./senseD3utils.js"], function ( qlik, d3 ) {
+
+	var control = false;
+	qlik.setOnError( function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		if ( !control ) {
+			control = true;
+			$( '#popup' ).delay( 1000 ).fadeIn( 1000 ).delay( 11000 ).fadeOut( 1000 );
+		}
+	} );
+
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+	if ( $( 'ul#qbmlist li' ).length === 0 ) {
+		$( '#qbmlist' ).append( "<li><a>No bookmarks available</a></li>" );
+	}
+	$( "body" ).css( "overflow: hidden;" );
+	function AppUi ( app ) {
+		var me = this;
+		this.app = app;
+		app.global.isPersonalMode( function ( reply ) {
+			me.isPersonalMode = reply.qReturn;
+		} );
+		app.getAppLayout( function ( layout ) {
+			$( "#title" ).html( layout.qTitle );
+			$( "#title" ).attr( "title", "Last reload:" + layout.qLastReloadTime.replace( /T/, ' ' ).replace( /Z/, ' ' ) );
+			//TODO: bootstrap tooltip ??
+		} );
+		app.getList( 'SelectionObject', function ( reply ) {
+			$( "[data-qcmd='back']" ).parent().toggleClass( 'disabled', reply.qSelectionObject.qBackCount < 1 );
+			$( "[data-qcmd='forward']" ).parent().toggleClass( 'disabled', reply.qSelectionObject.qForwardCount < 1 );
+		} );
+		app.getList( "BookmarkList", function ( reply ) {
+			var str = "";
+			reply.qBookmarkList.qItems.forEach( function ( value ) {
+				if ( value.qData.title ) {
+					str += '<li><a data-id="' + value.qInfo.qId + '">' + value.qData.title + '</a></li>';
+				}
+			} );
+			str += '<li><a data-cmd="create">Create</a></li>';
+			$( '#qbmlist' ).html( str ).find( 'a' ).on( 'click', function () {
+				var id = $( this ).data( 'id' );
+				if ( id ) {
+					app.bookmark.apply( id );
+				} else {
+					var cmd = $( this ).data( 'cmd' );
+					if ( cmd === "create" ) {
+						$( '#createBmModal' ).modal();
+					}
+				}
+			} );
+		} );
+		$( "[data-qcmd]" ).on( 'click', function () {
+			var $element = $( this );
+			switch ( $element.data( 'qcmd' ) ) {
+				//app level commands
+				case 'clearAll':
+					app.clearAll();
+					break;
+				case 'back':
+					app.back();
+					break;
+				case 'forward':
+					app.forward();
+					break;
+				case 'lockAll':
+					app.lockAll();
+					break;
+				case 'unlockAll':
+					app.unlockAll();
+					break;
+				case 'createBm':
+					var title = $( "#bmtitle" ).val(), desc = $( "#bmdesc" ).val();
+					app.bookmark.create( title, desc );
+					$( '#createBmModal' ).modal( 'hide' );
+					break;
+			}
+		} );
+	}
+	
+	
+
+	//callbacks -- inserted here --
+	function getStateData(reply, app){
+		//remove all d3 elements from svg
+		$('#QV01 svg').empty();
+		
+		var data = senseD3.createJSONObj(reply);
+		
+		var svg = d3.select("svg"),
+			margin = {top: 20, right: 20, bottom: 30, left: 40},
+			width = +svg.attr("width") - margin.left - margin.right,
+			height = +svg.attr("height") - margin.top - margin.bottom;
+
+		var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+			y = d3.scaleLinear().rangeRound([height, 0]);
+
+		var g = svg.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		x.domain(data.map(function(d) { return d.dim_0; }));
+		y.domain([0, d3.max(data, function(d) { return d.meas_0; })]);
+
+		g.append("g")
+			.attr("class", "axis axis--x")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+
+		g.append("g")
+				.attr("class", "axis axis--y")
+				.call(d3.axisLeft(y).ticks(10))
+			.append("text")
+				.attr("transform", "rotate(-90)")
+				.attr("y", 6)
+				.attr("dy", "0.71em")
+				.attr("text-anchor", "end")
+				.text("Income");
+
+		g.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+				.attr("class", "bar")
+				.attr("x", function(d) { return x(d.dim_0); })
+				.attr("y", function(d) { return y(d.meas_0); })
+				.attr("width", x.bandwidth())
+				.attr("height", function(d) { return height - y(d.meas_0); })
+				.on("click", function(d) {
+					app.field('addr_state').select([d.dim_0_id], true, true);
+				});
+		
+	}
+
+	//open apps -- inserted here --
+	var app = qlik.openApp('_MyExtensionPlayground.qvf', config);
+
+
+	//get objects -- inserted here --
+	//create cubes and lists -- inserted here --
+	app.createCube({
+	"qInitialDataFetch": [
+		{
+			"qHeight": 1000,
+			"qWidth": 2
+		}
+	],
+	"qDimensions": [
+		{
+			"qDef": {
+				"qFieldDefs": [
+					"addr_state"
+				]
+			},
+			"qNullSuppression": true,
+			"qOtherTotalSpec": {
+				"qOtherMode": "OTHER_OFF",
+				"qSuppressOther": true,
+				"qOtherSortMode": "OTHER_SORT_DESCENDING",
+				"qOtherCounted": {
+					"qv": "5"
+				},
+				"qOtherLimitMode": "OTHER_GE_LIMIT"
+			}
+		}
+	],
+	"qMeasures": [
+		{
+			"qDef": {
+				"qDef": "Sum(annual_inc)"
+			},
+			"qLabel": "Sum(annual_inc)",
+			"qLibraryId": null,
+			"qSortBy": {
+				"qSortByState": 0,
+				"qSortByFrequency": 0,
+				"qSortByNumeric": 0,
+				"qSortByAscii": 1,
+				"qSortByLoadOrder": 0,
+				"qSortByExpression": 0,
+				"qExpression": {
+					"qv": " "
+				}
+			}
+		}
+	],
+	"qSuppressZero": false,
+	"qSuppressMissing": false,
+	"qMode": "S",
+	"qInterColumnSortOrder": [],
+	"qStateName": "$"
+	},getStateData);
+if ( app ) {
+		new AppUi( app );
+	}
+
+} );
